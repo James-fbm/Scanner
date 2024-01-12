6b5614d3185e25797ed0e3d604f20656ecdbd1b2
James-fbm <1433170308@qq.com>
2024-01-11 22:23:31 -0800

    begin to implement rename dialog

---
+++ app/src/main/java/com/example/scanner/ui/component/home/ProjectRenameDialog.kt
@@ -0,0 +1,15 @@
+package com.example.scanner.ui.component.home
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.runtime.Composable
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ProjectRenameDialog(
+    onDialogVisibleChanged: () -> Unit
+) {
+    ModalBottomSheet(onDismissRequest = {}){
+
+    }
+}
