5f401bf1ac27b7caf1a6697ef3fcce1cc0c6fd41
James-fbm <1433170308@qq.com>
2024-01-12 14:13:14 -0800

    test dialog.

--- app/src/main/java/com/example/scanner/ui/component/home/ProjectModifyDialog.kt
+++ app/src/main/java/com/example/scanner/ui/component/home/ProjectModifyDialog.kt
@@ -1,17 +1,29 @@
 package com.example.scanner.ui.component.home
 
 import android.widget.Space
+import androidx.compose.foundation.focusable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.windowInsetsBottomHeight
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.BottomSheetDefaults
+import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.SheetState
+import androidx.compose.material3.SheetValue
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -20,32 +32,50 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
 import com.example.scanner.ui.viewmodel.ProjectItemUiModel
 
+// ModalBottomSheet has trouble with keyboard imePadding. DO NOT USE IT.
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ProjectModifyDialog(
     projectItemUiModel: ProjectItemUiModel?,
-    onDialogVisibleChanged: () -> Unit
+    onDialogVisibleChanged: () -> Unit,
 ) {
 
-    ModalBottomSheet(
-        onDismissRequest = onDialogVisibleChanged,
-        modifier = Modifier.fillMaxHeight()
-    ){
-        OutlinedTextField(
-            value = "",
-            onValueChange = {},
-            label = { Text("Name") },
-            singleLine = true,
-            placeholder = {
-                if ( projectItemUiModel == null )
-                    Text("Project Name")
-                else
-                    Text(projectItemUiModel.projectName)
-                          },
-            modifier = Modifier.padding(16.dp).fillMaxWidth()
-        )
+    var bottomSheetVisible = remember {
+        mutableStateOf(true)
+    }
+    if (bottomSheetVisible.value) {
+        Dialog (
+            onDismissRequest = { bottomSheetVisible.value = false },
+        ) {
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(200.dp)
+                    .padding(16.dp),
+                shape = RoundedCornerShape(16.dp),
+            ) {
+                OutlinedTextField(
+                    value = "",
+                    onValueChange = {},
+                    label = { Text("Name") },
+                    singleLine = true,
+                    placeholder = {
+                        if (projectItemUiModel == null)
+                            Text("Project Name")
+                        else
+                            Text(projectItemUiModel.projectName)
+                    },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(32.dp)
+                )
+            }
+        }
     }
 }
